51abe67208ad7411c6ff79eecdb5ba7f5e874e1f
kiloo34 <uchiharobi@rocketmail.com>
2021-06-10 22:40:31 +0700

    Landing Page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,4 +1,79 @@
 @extends('layouts.landing')
 @section('content')
-sdfi
+<div class="content">
+    <div class="title m-b-md">
+        SIMPELAN
+    </div>
+
+    <div class="links">
+        @auth
+        @if (auth()->user()->role->nama == 'mantri')
+        <a href="{{ route('mantri.dashboard') }}">{{__("Beranda")}}</a>
+        @else
+        <a href="{{ route('holtikultura.dashboard') }}"> {{__("Beranda")}}</a>
+        @endif
+        @else
+        <a href="{{ route('login') }}"> {{__("Login")}}</a>
+        {{-- <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li> --}}
+        @if (Route::has('register'))
+        <a href="{{ route('register') }}"> {{__("Register")}}</a>
+        @endif
+        @endauth
+    </div>
+</div>
 @endsection
+
+@push('styles')
+<style>
+    /* html, body {
+        background-color: #fff;
+        color: #636b6f;
+        font-family: 'Nunito', sans-serif;
+        font-weight: 200;
+        height: 100vh;
+        margin: 0;
+    } */
+
+    .full-height {
+        height: 100vh;
+    }
+
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    .position-ref {
+        position: relative;
+    }
+
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
+
+    .content {
+        text-align: center;
+    }
+
+    .title {
+        font-size: 84px;
+    }
+
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 13px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+</style>
+@endpush

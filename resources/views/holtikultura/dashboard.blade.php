4509006e04296fb2c8db6f4b82f7a52cc8fd7684
kiloo34 <uchiharobi@rocketmail.com>
2021-06-21 10:23:04 +0700

    Dashboard
    Aktor : Holtikultura
    Deskripsi : Update dashboard

--- resources/views/holtikultura/dashboard.blade.php
+++ resources/views/holtikultura/dashboard.blade.php
@@ -1,4 +1,130 @@
 @extends('layouts.myview')
 @section('content')
-<h1>masuk dashboard Hltikultura</h1>
+<div class="row">
+    @include('components.infobox', ['col'=>'col-md-4 col-sm-6', 'icon'=>'user', 'isi'=>count($kecamatan),
+    'judul'=>'kecamatan'])
+    @include('components.infobox', ['col'=>'col-md-4 col-sm-6', 'icon'=>'clock', 'isi'=>count($periode),
+    'judul'=>'periode'])
+</div>
+<div class="row">
+    <div class="col-md-6 col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h4>Data Harga Buah Naga</h4>
+            </div>
+            <div class="card-body">
+                <canvas id="chartharga"></canvas>
+            </div>
+        </div>
+    </div>
+    {{-- <div class="col-md-6 col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h4>Data Permintaan</h4>
+            </div>
+            <div class="card-body">
+                <canvas id="chartpermintaan"></canvas>
+            </div>
+        </div>
+    </div> --}}
+</div>
 @endsection
+@push('scripts')
+<script>
+    $(document).ready(function() {
+        var ctx1 = document.getElementById("chartharga").getContext('2d');
+        var myChart = new Chart(ctx1, {
+            type: 'bar',
+            data: {
+                labels: @json($label),
+                datasets: [
+                    @foreach ($chart as $c)
+                    {
+                        label: @json(ucfirst($c['kecamatan'])) ,
+                        data: @json($c['harga']),
+                        borderColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+                        backgroundColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+                        pointRadius: 4
+                    },
+                    @endforeach
+                ]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: @json(ucfirst($title)),
+                    }
+                },
+                legend: {
+                    display: true
+                },
+                interaction: {
+                    intersect: false,
+                    mode: 'index',
+                },
+                scales: {
+                    x: {
+                        stacked: true,
+                    },
+                    y: {
+                        stacked: true
+                    }
+                },
+            },
+        });
+
+
+    });
+</script>
+@endpush
+@include('import.chartjs')
+{{-- var ctx2 = document.getElementById("chartpermintaan").getContext('2d');
+        var myChart = new Chart(ctx2, {
+            type: 'bar',
+            data: {
+                labels: @json($label),
+                datasets: [
+                    @foreach ($chart as $c)
+                    {
+                        label: @json(ucfirst($c['kecamatan'])) ,
+                        data: @json($c['permintaan']),
+                        borderColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+                        backgroundColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+                        pointRadius: 4
+                    },
+                    @endforeach
+                ]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: @json(ucfirst($title)),
+                    }
+                },
+                legend: {
+                    display: true
+                },
+                interaction: {
+                    intersect: false,
+                    mode: 'index',
+                },
+                scales: {
+                    x: {
+                        stacked: true,
+                    },
+                    y: {
+                        stacked: true
+                    }
+                },
+            },
+        }); --}}
